4e8de8c909fa25d73e8f8e3c81b6ea769d5e628f
Nahom G <nohgebr@gmail.com>
2021-05-03 14:13:51 -0700

    FirstCommit

--- EmployementManagement/Startup.cs
+++ EmployementManagement/Startup.cs
@@ -5,12 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EmployementManagement
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        // Notice we are using Dependency Injection here
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +36,7 @@ namespace EmployementManagement
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(_configuration["MyKey"]);
             });
         }
     }
